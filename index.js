ebc61a0f4ca5ca07e91fb971d002bcb1dcdc8ca8
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-03-15 11:26:10 +0530

    Set interval to 10 seconds.

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const client = new twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-fetch("https://tintin-quotes-api.herokuapp.com/quotes/random")
-.then(res => res.json())
-.then(res => {
-  const quote = res;
+setInterval(function() {
+  fetch("https://tintin-quotes-api.herokuapp.com/quotes/random")
+  .then(res => res.json())
+  .then(res => {
+    const quote = res;
 
-  const full_tweet = quote.text + " - " + quote.author;
+    const full_tweet = quote.text + "\n\n - " + quote.author;
 
-  if (full_tweet.length < 280){
-    console.log("Less than 280: " + full_tweet);
-    client.post('statuses/update', {status: full_tweet},  function(error, tweet, response) {
-      if(error) throw error;
-    });
-  }
+    if (full_tweet.length < 280){
+      client.post('statuses/update', {status: full_tweet},  function(error, tweet, response) {
+        if(error) throw error;
+      });
+    }
 
-})
-.catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
+
+}, 10000);
