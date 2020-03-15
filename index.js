7982aeaed8be4043388865d94feaced360a580c2
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-03-15 10:32:05 +0530

    Initial commit.

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const twitter = require('twitter');
+const fetch = require('node-fetch');
+
+const client = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+fetch("https://tintin-quotes-api.herokuapp.com/quotes/random")
+.then(res => res.json())
+.then(res => {
+  const quote = res;
+
+  const full_tweet = quote.text + " - " + quote.author;
+
+  if (full_tweet.length < 280){
+    console.log("Less than 280: " + full_tweet);
+    client.post('statuses/update', {status: full_tweet},  function(error, tweet, response) {
+      if(error) throw error;
+    });
+  }
+
+})
+.catch(err => console.log(err));
