b49039f6d3900da079b85ec8eb26a1d5eafd481c
Upamanyu Das <47525983+tintindas@users.noreply.github.com>
2020-12-04 10:45:55 +0530

    delete setInterval

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// require modules
 require('dotenv').config()
 const twitter = require('twitter');
 const fetch = require('node-fetch');
 
+// authenticate twitter
 const client = new twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -9,68 +11,67 @@ const client = new twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-setInterval(function() {
 
-  // Fetch quotes from API
-  fetch("https://tintin-quotes-api.herokuapp.com/quotes/random")
-  .then(res => res.json())
-  .then(res => {
-    const quote = res;
+// Fetch quotes from API
+fetch("https://tintin-quotes-api.herokuapp.com/quotes/random")
+.then(res => res.json())
+.then(res => {
+  const quote = res;
 
-    const full_tweet = quote.text + "\n\n - " + quote.author;
+  const full_tweet = quote.text + "\n\n - " + quote.author;
 
-    // If quotes is less than tweet character limit post tweet
+  // If quotes is less than tweet character limit post tweet
 
-    if (full_tweet.length < 280){
-      client.post('statuses/update', {status: full_tweet},  function(error, tweet, response) {
-        if(error) throw error;
-        console.log("Tweeted: " + full_tweet);
-      });
+  //If tweet length is shorter than allowed limit  (280) characters then tweet directly
+  if (full_tweet.length < 280){
+    client.post('statuses/update', {status: full_tweet},  function(error, tweet, response) {
+      if(error) throw error;
+      console.log("Tweeted: " + full_tweet);
+    });
+  }
+  
+  
+  else{
+    
+    // Split into 280 character fragments
+    const words = full_tweet.split(" ");
+    const quoteFragments = [];
+    let quoteFragment = "";
+
+    words.forEach(word => {
+      quoteFragment += word + " ";
+      if(quoteFragment.length > 200){
+        quoteFragments.push(quoteFragment);
+        quoteFragment=" ";
+      }
+    });
+
+    quoteFragments.push(quoteFragment);
+
+    for(let i=0; i<quoteFragments.length-1; i++){
+      quoteFragments[i] += "....."
     }
 
-    else{
+    console.log(quoteFragments);
 
-      const words = full_tweet.split(" ");
-      const quoteFragments = [];
-      let quoteFragment = "";
+    
+    var prev_id = null;
 
-      words.forEach(word => {
-        quoteFragment += word + " ";
-        if(quoteFragment.length > 200){
-          quoteFragments.push(quoteFragment);
-          quoteFragment=" ";
-        }
-      });
+    //Tweet quote as thread
+    async function updateFeed(){
+      for (item of quoteFragments){
+        console.log("Previous Tweet ID: " + prev_id);
+        let tweet = await client.post("statuses/update", {status: item, in_reply_to_status_id: prev_id});
 
-      quoteFragments.push(quoteFragment);
-
-      for(let i=0; i<quoteFragments.length-1; i++){
-        quoteFragments[i] += "....."
+        prev_id = tweet.id_str;
+        console.log(tweet.text);
       }
 
-      console.log(quoteFragments);
-
-
-      var prev_id = null;
-
-
-      async function updateFeed(){
-        for (item of quoteFragments){
-          console.log("Previous Tweet ID: " + prev_id);
-          let tweet = await client.post("statuses/update", {status: item, in_reply_to_status_id: prev_id});
-
-          prev_id = tweet.id_str;
-          console.log(tweet.text);
-        }
-
-        console.log("done");
-      }
-
-      updateFeed();
+      console.log("done");
     }
 
-  })
-  .catch(err => console.log(err));
+    updateFeed();
+  }
 
-
-}, 10800000); //10800000ms = 3 hours
+})
+.catch(err => console.log(err));
