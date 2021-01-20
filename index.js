29ba6f305ce8d89bedbcfd21dd26f6bdbcb73421
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-01-20 21:33:36 +0530

    Add tags to tweets

--- index.js
+++ index.js
@@ -1,77 +1,87 @@
 // require modules
 require('dotenv').config()
-const twitter = require('twitter');
-const fetch = require('node-fetch');
+const twitter = require('twitter')
+const fetch = require('node-fetch')
 
 // authenticate twitter
 const client = new twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
 
 // Fetch quotes from API
-fetch("https://tintin-quotes-api.herokuapp.com/quotes/random")
-.then(res => res.json())
-.then(res => {
-  const quote = res;
+fetch('https://tintin-quotes-api.herokuapp.com/quotes/random')
+	.then((res) => res.json())
+	.then((res) => {
+		const { quoteText, author, source, tags } = res
 
-  const full_tweet = quote.text + "\n\n - " + quote.author;
+		let hashtags = ``
+		for (let i = 0; i < tags.length; i++) {
+			hashtags += `#${tags[i]} `
+		}
 
-  // If quotes is less than tweet character limit post tweet
+		const full_tweet = `${quoteText}
 
-  //If tweet length is shorter than allowed limit  (280) characters then tweet directly
-  if (full_tweet.length < 280){
-    client.post('statuses/update', {status: full_tweet},  function(error, tweet, response) {
-      if(error) throw error;
-      console.log("Tweeted: " + full_tweet);
-    });
-  }
-  
-  
-  else{
-    
-    // Split into 280 character fragments
-    const words = full_tweet.split(" ");
-    const quoteFragments = [];
-    let quoteFragment = "";
+        - ${author}, ${source}
 
-    words.forEach(word => {
-      quoteFragment += word + " ";
-      if(quoteFragment.length > 200){
-        quoteFragments.push(quoteFragment);
-        quoteFragment=" ";
-      }
-    });
+${hashtags}
+        `
 
-    quoteFragments.push(quoteFragment);
+		// If quotes is less than tweet character limit post tweet
 
-    for(let i=0; i<quoteFragments.length-1; i++){
-      quoteFragments[i] += "....."
-    }
+		//If tweet length is shorter than allowed limit  (280) characters then tweet directly
+		if (full_tweet.length < 280) {
+			client.post(
+				'statuses/update',
+				{ status: full_tweet },
+				function (error, tweet, response) {
+					if (error) throw error
+					console.log('Tweeted: ' + full_tweet)
+				}
+			)
+		} else {
+			// Split into 280 character fragments
+			const words = full_tweet.split(' ')
+			const quoteFragments = []
+			let quoteFragment = ''
 
-    console.log(quoteFragments);
+			words.forEach((word) => {
+				quoteFragment += word + ' '
+				if (quoteFragment.length > 200) {
+					quoteFragments.push(quoteFragment)
+					quoteFragment = ' '
+				}
+			})
 
-    
-    var prev_id = null;
+			quoteFragments.push(quoteFragment)
 
-    //Tweet quote as thread
-    async function updateFeed(){
-      for (item of quoteFragments){
-        console.log("Previous Tweet ID: " + prev_id);
-        let tweet = await client.post("statuses/update", {status: item, in_reply_to_status_id: prev_id});
+			for (let i = 0; i < quoteFragments.length - 1; i++) {
+				quoteFragments[i] += '.....'
+			}
 
-        prev_id = tweet.id_str;
-        console.log(tweet.text);
-      }
+			console.log(quoteFragments)
 
-      console.log("done");
-    }
+			var prev_id = null
 
-    updateFeed();
-  }
+			//Tweet quote as thread
+			async function updateFeed() {
+				for (item of quoteFragments) {
+					console.log('Previous Tweet ID: ' + prev_id)
+					let tweet = await client.post('statuses/update', {
+						status: item,
+						in_reply_to_status_id: prev_id
+					})
 
-})
-.catch(err => console.log(err));
+					prev_id = tweet.id_str
+					console.log(tweet.text)
+				}
+
+				console.log('done')
+			}
+
+			updateFeed()
+		}
+	})
+	.catch((err) => console.log(err))
